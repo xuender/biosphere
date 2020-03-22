a54945973f39f9cfb44242af5d41cd180a774ba6
ender <xuender@gmail.com>
2020-03-22 08:03:13 +0800

    life game

--- _examples/life/main/main.go
+++ _examples/life/main/main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	b := biosphere.NewBiosphere(&life.Life{})
-	b.EvalTimes = 3000
-	b.TryTimes = 50
+	b.EvalTimes = 5000
+	b.TryTimes = 66
 	b.VariationTimes = 5
 	b.Run()
 	b.Best(3)
